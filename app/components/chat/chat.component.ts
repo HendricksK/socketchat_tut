9933c110f09bc88ca34b3cc6466ff8989d3fc139
kurvin <kurvin@klarity.co.za>
2017-01-11 13:39:15 +0200

    Adding async messaging, username functionality using session storage for now

--- app/components/chat/chat.component.ts
+++ app/components/chat/chat.component.ts
@@ -10,13 +10,15 @@ export class ChatComponent implements OnInit, OnDestroy {
     messages: any = [];
     message:string;
     connection: any;
+    username: string;
+    alert: any = false;
 
     constructor(private _chatService:ChatService) {
 
     }
 
     sendMessage() {
-        this._chatService.sendMessage(this.message);
+        this._chatService.sendMessage(this.message, this.username);
         this.message = '';
     }
 
@@ -29,5 +31,10 @@ export class ChatComponent implements OnInit, OnDestroy {
 
     ngOnDestroy() {
         this.connection.unsubscribe();
+    }
+
+    setUsername() {
+        this._chatService.setUsername(this.username);
+        this.alert = 'Username is set';
     }
 }
